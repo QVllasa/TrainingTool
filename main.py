8cf2fe353e3113c5338e6e35c534a880e9bfb7c3
Qendrim Vllasa <qendrim.vllasa@siemens.com>
2019-05-06 11:59:08 +0200

    inital upload

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import sys
 
-
 from PyQt5.QtCore import *
 from qtpy.QtWidgets import *
 
 from ui.mainwindow import Ui_MainWindow
 
 app = QApplication(sys.argv)
+
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -18,6 +18,21 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
 
+        self.ui.addPart.clicked.connect(self.addCell)
+
+    def addCell(self):
+        row = self.ui.materialList.rowCount()
+        self.ui.materialList.insertRow(row)
+
+
+class Worker(QThread):
+    blabla = pyqtSignal(str)
+
+    def __init__(self):
+        QThread.__init__(self)
+
+    def run(self) -> None:
+        pass
 
 window = MainWindow()
 window.show()
