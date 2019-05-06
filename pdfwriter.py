096b0b9aee4b632c128d05e0b98b84f9b29b650d
QendrimVllasa <qendrimvllasa@hotmail.de>
2019-05-07 00:25:21 +0200

    add watermark to all pdf pages

--- pdfwriter.py
+++ pdfwriter.py
@@ -5,41 +5,38 @@ from reportlab.lib.colors import Color, black, blue, red
 
 buffer = BytesIO()
 file = open('files/Material/PDF/Basic_App_Dev_Training_V2.3.pdf', 'rb')
-pdf = PdfFileReader(file)
-x = pdf.getPage(0).mediaBox[-2]
-y = pdf.getPage(0).mediaBox[-1]
+material = PdfFileReader(file)
+x = material.getPage(0).mediaBox[-2]
+y = material.getPage(0).mediaBox[-1]
+
+pageNum = material.getNumPages()
 
 
 
-# create a new PDF with Reportlab
 p = canvas.Canvas(buffer)
 r = Color(0,0,0, alpha = 0.5)
 p.setFont('Helvetica', 75)
 p.setFillColor(r)
 p.setPageSize((x, y))
-#p.saveState()
+
 p.translate(x/2, y/2)
 p.rotate(45)
 p.drawCentredString(0, 0, "holaaaaaaaaaaaa")
-#p.restoreState()
-p.showPage() #so anything you draw on the canvas after calling it will go on the next page.
+
+p.showPage()
 p.save()
-
-#move to the beginning of the StringIO buffer
 buffer.seek(0)
-newPdf = PdfFileReader(buffer)
 
-#######DEBUG NEW PDF created#############
-pdf1 = buffer.getvalue()
-open('pdf1.pdf', 'wb').write(pdf1)
-#########################################
-# read your existing PDF
-
+watermark = PdfFileReader(buffer)
 output = PdfFileWriter()
 # add the "watermark" (which is the new pdf) on the existing page
-page = pdf.getPage(0)
-page.mergePage(newPdf.getPage(0))
-output.addPage(page)
+
+for page in range(pageNum):
+    slide = material.getPage(page)
+    slide.mergePage(watermark.getPage(0))
+    output.addPage(slide)
+
+
 # finally, write "output" to a real file
 outputStream = open('output.pdf', 'wb')
 output.write(outputStream)
