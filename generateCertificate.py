a48787cd4fe2e3c6a1ca944ea2312076e30cd65c
QendrimVllasa <qendrimvllasa@hotmail.de>
2019-05-08 01:21:20 +0200

    create script for certificates

--- generateCertificate.py
+++ generateCertificate.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from reportlab.pdfgen import canvas
 from reportlab.lib.units import cm
 from reportlab.lib.pagesizes import A4
+from reportlab.graphics.shapes import *
 from reportlab.lib.colors import Color
 from compressor import compress
 from reportlab.pdfbase import pdfmetrics
@@ -10,11 +11,11 @@ from reportlab.pdfbase.ttfonts import TTFont
 import os
 
 pdfmetrics.registerFont(TTFont('Siemens Sans03', 'misc/Siemens_Sans/SISAN03.ttf'))
-pdfmetrics.registerFont(TTFont('Siemens Sans', 'misc/Siemens_Sans/SISAN006.ttf'))
-pdfmetrics.registerFont(TTFont('Siemens Sans', 'misc/Siemens_Sans/SISAN03.ttf'))
-pdfmetrics.registerFont(TTFont('Siemens Sans', 'misc/Siemens_Sans/SISAN03.ttf'))
-pdfmetrics.registerFont(TTFont('Siemens Sans', 'misc/Siemens_Sans/SISAN03.ttf'))
-pdfmetrics.registerFont(TTFont('Siemens Sans', 'misc/Siemens_Sans/SISAN03.ttf'))
+pdfmetrics.registerFont(TTFont('Siemens Sans06', 'misc/Siemens_Sans/SISAN06.ttf'))
+pdfmetrics.registerFont(TTFont('Siemens Sans08', 'misc/Siemens_Sans/SISAN08.ttf'))
+pdfmetrics.registerFont(TTFont('Siemens Sans33', 'misc/Siemens_Sans/SISAN33.ttf'))
+pdfmetrics.registerFont(TTFont('Siemens Sans36', 'misc/Siemens_Sans/SISAN36.ttf'))
+pdfmetrics.registerFont(TTFont('Siemens Sans38', 'misc/Siemens_Sans/SISAN38.ttf'))
 
 
 
@@ -31,18 +32,28 @@ pageNum = material.getNumPages()
 p = canvas.Canvas(buffer, pagesize=A4)
 #r = Color(0, 0, 0)
 #r = Color(55, 53, 53)
-p.setFont('Helvetica', 10)
+p.setFont('Siemens Sans06', 10)
 p.setFillColorRGB(55/255,53/255,53/255)
 
 width, height = A4
-p.setFillColorRGB(0,0,0)
-p.drawString(1.7*cm, 14.65*cm, "<<ParticipantFName>>")
-p.drawString(1.7*cm, 14.225*cm, "<<ParticipantLName>>")
+p.setFillColorRGB(1,1,1)
 
+p.rect(1.7*cm,13*cm, 5*cm, 2*cm,stroke=0, fill=1)
+p.setFillColorRGB(0,0,0)
+
+
+p.drawString(1.7*cm, 14.65*cm, "Qendrim")
+p.drawString(1.7*cm, 14.225*cm, "Vllasa")
+
+p.setFont('Siemens Sans03', 10)
 p.setFillColorRGB(55/255,53/255,53/255)
-p.drawString(1.7*cm, 13.54*cm, "Date: <<Training_Date>>")
-p.drawString(1.7*cm, 13.125*cm, "Place: <<TrainingLocation>>")
-p.drawString(14*cm,4.7*cm,"<<Trainer>>")
+p.drawString(1.7*cm, 13.54*cm, "Date: 25. - 28.04.2019")
+p.drawString(1.7*cm, 13.125*cm, "Place: Karlsruhe, Germany")
+
+p.setFillColorRGB(1,1,1)
+p.rect(13.5*cm,4.6*cm, 4*cm, 1*cm,stroke=0, fill=1)
+p.setFillColorRGB(55/255,53/255,53/255)
+p.drawString(13.925*cm,4.7*cm,"Max Mustermann")
 
 
 p.showPage()
