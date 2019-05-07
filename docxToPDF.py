b78a0d29c8ab1a6c101a1db2cd2e2c5030b062c0
QendrimVllasa <qendrimvllasa@hotmail.de>
2019-05-07 20:12:10 +0200

    add script for converting docx to pdf

--- docxToPDF.py
+++ docxToPDF.py
@@ -1,18 +1,16 @@
-import win32compat.client as client
+import sys
+import os
+import comtypes.client
+
+wdFormatPDF = 17
+
+in_file = 'files/new version/Certification/MindSphere_Academy_attendance_certificate_Basic_application_Development_training_V2.docx'
+out_file = 'temp/Certification/MindSphere_Academy_attendance_certificate_Basic_application_Development_training_V2.docx'
+
+word = comtypes.client.CreateObject('Word.Application')
+doc = word.Documents.Open(in_file)
+doc.SaveAs(out_file, FileFormat=wdFormatPDF)
+doc.Close()
+word.Quit()
 
 
-def convert_to_pdf(filepath:str):
-    """Save a pdf of a docx file."""
-    try:
-        word = client.DispatchEx("Word.Application")
-        target_path = filepath.replace(".docx", r".pdf")
-        word_doc = word.Documents.Open(filepath)
-        word_doc.SaveAs(target_path, FileFormat=17)
-        word_doc.Close()
-    except Exception as e:
-            raise e
-    finally:
-            word.Quit()
-
-
-convert_to_pdf('files/new version/Certification/MindSphere_Academy_attendance_certificate_Basic_application_Development_training_V2.docx')
