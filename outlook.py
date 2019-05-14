c59f2ac6a68fe4609ff995970def858622dafa9a
QendrimVllasa <qendrimvllasa@hotmail.de>
2019-05-14 22:26:05 +0200

    working outlook app

--- outlook.py
+++ outlook.py
@@ -1,2 +1,19 @@
-import win32com.client as win32
+import win32com.client
 
+def send_mail_via_com(text, subject, recipient, profilename="Outlook2003"):
+
+    o = win32com.client.Dispatch("Outlook.Application")
+
+
+    Msg = o.CreateItem(0)
+    Msg.To = recipient
+
+    Msg.Subject = subject
+    Msg.Body = text
+
+    # attachment1 = "Path to attachment no. 1"
+    # attachment2 = "Path to attachment no. 2"
+    # Msg.Attachments.Add(attachment1)
+    # Msg.Attachments.Add(attachment2)
+
+    Msg.Display()
