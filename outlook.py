39bacdafe479878c2128cf7b6f3a162469f44467
QendrimVllasa <qendrimvllasa@hotmail.de>
2019-05-26 23:05:51 +0200

    finishing

--- outlook.py
+++ outlook.py
@@ -1,21 +1,21 @@
-import win32com.client
+#import win32com.client
 
 def send_mail_via_com(text, subject, recipient, certificatepath, materialpath):
 
-    o = win32com.client.Dispatch("Outlook.Application")
-
-
-    Msg = o.CreateItem(0)
-    Msg.To = recipient
-
-    Msg.Subject = subject
-    Msg.Body = text
-
-
-    Msg.Attachments.Add(certificatepath)
-    Msg.Attachments.Add(materialpath)
-
-    Msg.Display()
+    # o = win32com.client.Dispatch("Outlook.Application")
+    #
+    #
+    # Msg = o.CreateItem(0)
+    # Msg.To = recipient
+    #
+    # Msg.Subject = subject
+    # Msg.Body = text
+    #
+    #
+    # Msg.Attachments.Add(certificatepath)
+    # Msg.Attachments.Add(materialpath)
+    #
+    # Msg.Display()
     print('E-Mail Beginn---------------------')
     print(certificatepath)
     print(materialpath)
